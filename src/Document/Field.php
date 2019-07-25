0c1a98216831c7aa9bdf230f09d1fe202d0596de
Maciek Kostrzycki <maciej.kostrzycki@gmail.com>
2019-07-25 19:25:00 +0200

    Added str to lower for text_search type field value.

--- src/Document/Field.php
+++ src/Document/Field.php
@@ -136,7 +136,7 @@ class Field extends AbstractObject implements FieldInterface
                 case self::FIELD_TYPE_BOOL:
                     return (bool)$this->value;
                 case self::FIELD_TYPE_TEXT_SEARCH:
-                    return FieldHelper::alphanum($this->value);
+                    return mb_strtolower(FieldHelper::alphanum($this->value, true));
             }
         }
 
